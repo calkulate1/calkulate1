ea53e8f33617ccd9d6e6f3734f0e19d862096038
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2023-11-08 22:38:20 +0300

    dz.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-shopping_list = ["яблоко", "молоко", "хлеб", "яйца", "сок"]
-
-for person in shopping_list:
-    print(person)
-del shopping_list[1]
-shopping_list[0] = 'банан'
-print(shopping_list)
-a = len(shopping_list)
-print(a)
+n = int(input())
+k = int(input())-1
+people = list(range(1,n+1))
+while len(people) !=1:
+    k = k % len(people)
+    del people[k]
+    k += 2
+print(people)
