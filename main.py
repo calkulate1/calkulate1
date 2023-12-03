6c92093651589877061fadcb994701572b62039b
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2023-12-03 22:54:16 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-text = "пример для подсчета гласных букв"
-f = 0
-for i in text:
-    if i == 'а' or i == 'е' or i == 'и' or i == 'о' or i =='ы' or i == 'у' or i == 'я':
-        f += 1
-print(f)
+import random
+in1 = int(input('введите 1-ое число \n'))
+in2 = int(input('введите 2-ое число \n'))
+in3 = input('введите знак /, +, *, ** \n')
+a = random.randint(0,1)
+def plus():
+    return print(f"{in1} + {in2} = {in1 + in2}")
+
+def minus():
+    return print(f"{in1} - {in2} = {in1 - in2}")
+
+def umnogenie():
+    return print(f"{in1} * {in2} = {in1 * in2}")
+
+def delenie():
+    if in2 == 0:
+        print('ошибка, на 0 делить нельзя>:(')
+    else:
+        return print(f"{in1} / {in2} = {in1 / in2}")
+
+def stepen1():
+    return print(f"{in1} в степени {in2} = {in1 ** in2}")
+def stepen2():
+    return print(f"{in1} ** {in2} = {in1 ** in2}")
+if in3 == '+':
+    plus()
+elif in3 == '-':
+    minus()
+elif in3 == '*':
+    umnogenie()
+elif in3 =='/':
+    delenie()
+elif in3 == '**':
+    if a == 0:
+        stepen1()
+    else:
+        stepen2()
