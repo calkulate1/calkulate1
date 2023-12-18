967f056bc0d09299aeb1c5128c6eb698f83f2d07
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2023-12-18 17:13:08 +0300

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def niker(*args):
-    b = []
-    for a in args:
-        if isinstance(a, int) or isinstance(a, float):
-            b.append('.' * a)
-    return print(b)
+import time
 
 
-niker(1, 2, 2, 8, 8, 9, 2)
+def calculate_time(func):
+    def funs(a):
+        t = time.time()
+        funsc = func(a)
+        t2 = time.time()
+        return funsc, print(round(t2-t,2))
+    return funs
 
+@calculate_time
+def some_function(numbers):
+    spisok = []
+    for i in range(1, numbers + 1):
+        str_numbers = str(i)
+        spisok.append(i)
+some_function(int(input()))
